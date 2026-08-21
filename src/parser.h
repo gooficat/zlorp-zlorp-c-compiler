1d19e91f8a5c2f801338205e69c00ed777d69d64
Hasan Sabri <hsabri2007@gmail.com>
2026-08-21 01:10:37 +0100

    layouts

--- src/parser.h
+++ src/parser.h
@@ -1,4 +1,46 @@
 #ifndef __PARSER__H__
 #define __PARSER__H__
 
+enum c_type_type {
+    C_TYPE_NONE,
+    C_TYPE_ERR,
+    C_TYPE_VOID,
+    C_TYPE_INT,
+    C_TYPE_CHAR,
+    C_TYPE_FLT,
+    C_TYPE_PTR,
+    C_TYPE_ARRAY,
+    C_TYPE_STRUC_OR_UNION,
+
+};
+
+struct c_type {};
+
+enum c_storage {
+    C_STORAGE_NONE,
+    C_STORAGE_STATIC,
+    C_STORAGE_AUTO,
+    C_STORAGE_REGISTER,
+    C_STORAGE_TYPEDEF,
+    C_STORAGE_EXTERN,
+};
+
+struct c_var {};
+
+struct ast_expr {};
+
+enum ast_stmt_type {
+    AST_STMT_EMPTY,
+    AST_STMT_EXPR,
+    AST_STMT_ORDER,
+    AST_STMT_DECL,
+};
+
+struct ast_stmt {
+
+};
+
+
+void parse_tree(void);
+
 #endif
