b0dbd5327ef7af0f8d80b6d37a34d8099f89be2d
Hasan Sabri <hsabri2007@gmail.com>
2026-08-27 18:24:09 +0100

    preparing to write parser

--- src/main.c
+++ src/main.c
@@ -1,19 +1,11 @@
 #include "lexer.h"
+#include "parser.h"
 #include "pool.h"
 #include "tokens.h"
 
 int main(void) {
 	lexer_open(fopen("./test/1.c", "r"));
-
-	while (lexer.token.type != TK_EOF) {
-		if (lexer.token.type < TK_IDENT) {
-			printf("tok `%s`\n", TOKENS[lexer.token.type]);
-		} else if (lexer.token.type == TK_IDENT) {
-			printf("ident `%s`\n", identifiers.val[lexer.token.val]);
-		} else {
-			printf("tok `%i`\n", lexer.token.type);
-		}
-		lexer_next();
-	}
+	parse_tree();
+	print_tree();
 	lexer_close();
 }
--- src/parser.h
+++ src/parser.h
@@ -1,68 +1,66 @@
 #ifndef __PARSER__H__
 #define __PARSER__H__
 
-#include <stddef.h>
 #include "main.h"
-
+#include <stddef.h>
 
 enum ast_expr_type {
-    AST_EXPR_VREF,
-    AST_EXPR_UNARY,
-    AST_EXPR_BINARY,
-    AST_EXPR_CALL,
-    AST_EXPR_CONSTANT,
+	AST_EXPR_VREF,
+	AST_EXPR_UNARY,
+	AST_EXPR_BINARY,
+	AST_EXPR_CALL,
+	AST_EXPR_CONSTANT,
 };
 
 struct ast_expr_list {
-    struct ast_expr *val;
-    size_t len;
+	struct ast_expr *val;
+	size_t len;
 };
 
 struct ast_un_op {
-    
-    struct ast_expr *expr;
+
+	struct ast_expr *expr;
 };
 
-struct ast_bi_op {
-
-};
+struct ast_bi_op {};
 
 struct ast_call {};
 
 struct ast_expr {
-    enum ast_expr_type type;
+	enum ast_expr_type type;
 };
 
 enum ast_stmt_type {
-    AST_STMT_EMPTY,
-    AST_STMT_EXPR,
-    AST_STMT_ORDER,
-    AST_STMT_DECL,
+	AST_STMT_EMPTY,
+	AST_STMT_EXPR,
+	AST_STMT_ORDER,
+	AST_STMT_DECL,
 };
 
 struct ast_decl {
-    struct c_var *var;
-    struct ast_node *val;
+	struct c_var *var;
+	struct ast_node *val;
 };
 
 union ast_stmt_val {
-    struct ast_expr expr;
+	struct ast_expr expr;
 };
 
 struct ast_stmt {
-    enum ast_stmt_type type;
-    union ast_stmt_val value;
+	enum ast_stmt_type type;
+	union ast_stmt_val value;
 };
 
 struct ast_decl_list {
-    struct ast_decl *val;
-    size_t len;
+	struct ast_decl *val;
+	size_t len;
 };
 
 struct ast {
-    struct ast_decl_list decls;
+	struct ast_decl_list decls;
 };
 
 void parse_tree(void);
+void print_tree(void);
 
 #endif
